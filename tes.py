8c8a7e909f74e1203ca2fe425579d159bf460e55
Moussa <ibrahimmoumounimoussa@yahoo.fr>
2023-01-09 21:58:20 -0500

    new on main

--- tes.py
+++ tes.py
@@ -3,8 +3,10 @@ from time import perf_counter
 start=perf_counter()
 def do_something():
     print('sleep one second')
+    print('sleep one second 1')
+    print('sleep one second 2')
     time.sleep(1)
-    
+
 do_something()
 finish=pref_counter()
 print(f'Finished in round{(finish-start,2)} second()')
